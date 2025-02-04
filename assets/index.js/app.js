638ccf1aaf71e337d5020ea1a593d61a7fc224b2
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-02-04 19:25:08 +0530

    slider done

--- assets/index.js/app.js
+++ assets/index.js/app.js
@@ -5,3 +5,34 @@ const navbarbtn = () => {
     document.getElementById("rotates-2").classList.toggle("rotate-43")
     document.body.classList.toggle("overflow-hidden")
 }
+
+
+$('.happy-customer-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots: true,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            }
+        },
+
+
+    ]
+});
